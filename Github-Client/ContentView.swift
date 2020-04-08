c2cb945183492d47b9a2c974d4f618ad7d0f5b91
Hiroya Hinomori <nexearth@gmail.com>
2020-04-08 23:19:55 +0900

    Initial commit

---
+++ Github-Client/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Github-Client
+//
+//  Created by 日野森寛也 on 2020/04/08.
+//  Copyright © 2020 Hiroya Hinomori. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
