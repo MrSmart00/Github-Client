7fc74f0c91d18286b9c719bcd5d0c9a1a4742967
Hiroya Hinomori <nexearth@gmail.com>
2020-04-12 19:20:35 +0900

    a

---
+++ Github-Client/Secret.swift
@@ -0,0 +1,13 @@
+//
+//  Secret.swift
+//  Github-Client
+//
+//  Created by 日野森寛也 on 2020/04/10.
+//  Copyright © 2020 Hiroya Hinomori. All rights reserved.
+//
+
+import Foundation
+
+enum Secret {
+    static let githubToken = "<YOUR_GITHUB_TOKEN>"
+}
